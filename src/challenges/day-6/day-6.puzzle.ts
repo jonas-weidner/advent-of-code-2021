fb841f30ba7da641a50d005b7697e37b2a0906b5
Jonas Weidner <jonas@deity.io>
2021-12-06 07:57:12 +0100

    feat: Day 6 cleanup

--- src/challenges/day-6/day-6.puzzle.ts
+++ src/challenges/day-6/day-6.puzzle.ts
@@ -2,17 +2,16 @@ import fs from 'fs'
 import path from 'path'
 
 const prepareData = (file: string) => {
-  const fish = fs
-    .readFileSync(path.join(__dirname, file)).toString().split(',').map(s => parseInt(s.trim()))
+  const fish = fs.readFileSync(path.join(__dirname, file)).toString().split(',').map(s => parseInt(s))
   return Array.from(Array(9), (_, i) => fish.filter(f => f === i).length)
 }
 
 export const day6Puzzle = (file: string, days: number = 80) => {
   const fish = prepareData(file)
-  for (let day = 0; day < days; day++) {
-    const hasSpawnNew = fish.shift()
-    fish[6] = fish[6] + hasSpawnNew!
-    fish.push(hasSpawnNew!)
-  }
+  Array.from(Array(days)).forEach(() => {
+    const fishThatSpawnNewFish = fish.shift()
+    fish[6] += fishThatSpawnNewFish!
+    fish.push(fishThatSpawnNewFish!)
+  })
   return fish.reduce((sum, curr) => sum+curr, 0)
 }
