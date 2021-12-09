36aefee9a9c78bdd8ffb416a6f9c89f45e72c14a
Jonas Weidner <jonas@deity.io>
2021-12-10 00:00:54 +0100

    feat: Day 9 completed

--- src/challenges/day-9/day-9.puzzle.ts
+++ src/challenges/day-9/day-9.puzzle.ts
@@ -1,16 +1,39 @@
 import fs from 'fs'
 import path from 'path'
 
-const prepareData = (file: string) => fs
-  .readFileSync(path.join(__dirname, file)).toString().split('\n').filter(l => l.length).map(l => {
-    return l.split('').map(c => parseInt(c))
-  })
+const prepareData = (file: string) => fs.readFileSync(path.join(__dirname, file)).toString().split('\n')
+  .filter(l => l.length).map(l => l.split('').map(c => parseInt(c)))
 
-export const day9Puzzle1 = (file: string) => prepareData(file).reduce((risk, line, lineIdx, arr) => {
-  return risk + line.reduce((lineRisk, height, colIdx) => {
-    if (line[colIdx+1] <= height || line[colIdx-1] <= height) { return lineRisk }
-    if (arr[lineIdx-1] && arr[lineIdx-1][colIdx] <= height) { return lineRisk }
-    if (arr[lineIdx+1] && arr[lineIdx+1][colIdx] <= height) { return lineRisk }
-    return lineRisk + 1 + height
-  }, 0)
-}, 0)
+const lowPoints = (file: string, lines: number[][]) => lines.reduce((lineCoordinates, line, lineIdx, arr) => {
+  return [...lineCoordinates, ...line.reduce((colCoordinates, height, colIdx) => {
+    if (line[colIdx+1] <= height || line[colIdx-1] <= height) { return colCoordinates }
+    if (arr[lineIdx-1] && arr[lineIdx-1][colIdx] <= height) { return colCoordinates }
+    if (arr[lineIdx+1] && arr[lineIdx+1][colIdx] <= height) { return colCoordinates }
+    return [...colCoordinates, [lineIdx, colIdx]]
+  }, [] as number [][])]
+}, [] as number[][])
+
+
+export const day9Puzzle1 = (file: string) => {
+  const lines = prepareData(file)
+  return lowPoints(file, prepareData(file)).reduce((risk, point) => risk + 1 + lines[point[0]][point[1]], 0)
+}
+
+const go = (cs: number[][], lines: number[][]): number[][] => {
+  const lineIdx = cs[cs.length-1][0]
+  const colIdx = cs[cs.length-1][1]
+  const unique = (arr: number[][]) => arr.filter(c => !cs.find(co => co[0] === c[0] && co[1]===c[1]))
+  const condition = (lIdx: number, cIdx: number) => {
+    return !!lines[lIdx] && !!lines[lIdx][cIdx] && lines[lIdx][cIdx] !== 9 && !cs.find(c => c[0]===lIdx && c[1]===cIdx)
+  }
+  [[lineIdx-1, colIdx], [lineIdx+1, colIdx], [lineIdx, colIdx-1], [lineIdx, colIdx+1]].forEach(c => {
+    if (condition(c[0], c[1])) { cs.push(...unique(go([...cs, c], lines))) }
+  })
+  return cs
+}
+
+export const day9Puzzle2 = (file: string) => {
+  const lines = prepareData(file)
+  const basins = lowPoints(file, lines).map(point => go([point], lines).length).sort((a, b) => b-a)
+  return basins[0] * basins[1] * basins[2]
+}
--- src/challenges/day-9/day-9.test.ts
+++ src/challenges/day-9/day-9.test.ts
@@ -1,4 +1,4 @@
-import { day9Puzzle1 } from './day-9.puzzle'
+import { day9Puzzle1, day9Puzzle2 } from './day-9.puzzle'
 
 describe('Day 9', () => {
   it('Puzzle 1', () => {
@@ -6,8 +6,8 @@ describe('Day 9', () => {
     expect(day9Puzzle1('./data/input.txt')).toBe(478)
   })
 
-  // it('Puzzle 2', () => {
-  //   expect(day8Puzzle2('./data/mock.txt')).toBe(61229)
-  //   expect(day8Puzzle2('./data/input.txt')).toBe(1031553)
-  // })
+  it('Puzzle 2', () => {
+    expect(day9Puzzle2('./data/mock.txt')).toBe(1134)
+    expect(day9Puzzle2('./data/input.txt')).toBe(1327014)
+  })
 })
